465bb987c07ea27568c270a3038a3a8e771fe94b
danesgi <62260377+danesgi@users.noreply.github.com>
2022-09-27 19:27:58 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        JENKINS_URL:'http://ec2-54-89-195-189.compute-1.amazonaws.com:8080',
-        JENKINS_TOKEN:'11891a0b6fd4c37723ec6aace2c4fa7791'
+        JENKINS_URL:'',
+        JENKINS_TOKEN:''
     },
   }
